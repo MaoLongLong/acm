e7aa36ea08eddc4d2718caa76290bd1c7dd2bdb7
MaoLongLong <382084620@qq.com>
2020-08-31 23:25:21 +0800

    Add base.cpp

---
+++ base.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#define INF 0x3f3f3f3f
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+
+    return 0;
+}
